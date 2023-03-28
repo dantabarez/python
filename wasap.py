f589f492223ecec9667e6b5699df80204cafb4bc
dantabarez <dan.tabarez@gmail.com>
2023-03-28 11:54:00 -0500

    programa simple en python para enviar msgs

---
+++ wasap.py
@@ -0,0 +1,5 @@
+import pywhatkit
+import time
+
+#evio de mensajes por whatsapp usando python
+pywhatkit.sendwhatmsg("+593xxxxxxxxx", "cosita monita bebe fiu fiu", time.localtime().tm_hour, time.localtime().tm_min + 1)
