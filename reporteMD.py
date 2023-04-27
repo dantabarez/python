1f8a59f9077600a0548424f5b6c5e176918745a8
dantabarez <dan.tabarez@gmail.com>
2023-04-27 17:09:03 -0500

    generador de reportes de md a pdf

--- reporteMD.py
+++ reporteMD.py
@@ -26,6 +26,14 @@ Tareas:
 | 002    | Tarea 2      |
 | 003    | Tarea 3      |
 | 004    | Tarea 4      |
+
+
+| Column 1 | Column 2 | Column 3 |
+|----------|----------|----------|
+| Row 1    | Cell 2   | Cell 3   |
+| Row 2    | Cell 5   | Cell 6   |
+| Row 3    | Cell 8   | Cell 9   |
+
 """
 
 # Convierte el Markdown a HTML
